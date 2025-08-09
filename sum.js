f5dbd125c2151f53ab9f8101ad8ff4154169a8c0
Molina Guaico Gustavo Stiven <99373146+gsMolina02@users.noreply.github.com>
2025-08-09 15:23:58 -0500

    Mejora en el funcion sum

--- sum.js
+++ sum.js
@@ -1,4 +1,7 @@
-function sum(a, b){
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('Ambos argumentos deben ser números');
+    }
     return a + b;
 }
 
